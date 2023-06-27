87a50d901337025326b387f0ce2efef4e15de71f
Chetan Raj <bennchetan@gmail.com>
2023-06-27 12:23:33 -0500

    Updated Readme

--- src/components/Header.js
+++ src/components/Header.js
@@ -58,24 +58,7 @@ class Header extends Component {
                 height={40}
                 uncheckedIcon={
                   <span
-                    
-                  className="iconify"
-                  data-icon="heroicons:moon-solid"
-                  data-inline="false"
-                  style={{
-                    display: "block",
-                    height: "100%",
-                    fontSize: 25,
-                    textAlign: "end",
-                    marginLeft: "20px",
-                    color: "#ffffff",
-                  }}
-                  ></span>
-                }
-                checkedIcon={
-                  
-                  <span
-                  className="iconify"
+                    className="iconify"
                     data-icon="noto-v1:sun-with-face"
                     data-inline="false"
                     style={{
@@ -85,6 +68,22 @@ class Header extends Component {
                       textAlign: "end",
                       marginLeft: "10px",
                       color: "#353239",
+                    }}
+                  ></span>
+                }
+                checkedIcon={
+                  
+                  <span
+                    className="iconify"
+                    data-icon="heroicons:moon-solid"
+                    data-inline="false"
+                    style={{
+                      display: "block",
+                      height: "100%",
+                      fontSize: 25,
+                      textAlign: "end",
+                      marginLeft: "20px",
+                      color: "#ffffff",
                     }}
                   ></span>
                 }
