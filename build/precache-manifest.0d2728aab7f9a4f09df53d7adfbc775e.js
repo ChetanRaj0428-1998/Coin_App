4a7849618ed3a8f85f5ae91af3389cddd1e265eb
Chetan Raj <bennchetan@gmail.com>
2023-06-26 22:32:18 -0500

    Updated JSON

---
+++ build/precache-manifest.0d2728aab7f9a4f09df53d7adfbc775e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78806c9926fd95dad9f9407b0bc5276b",
+    "url": "/ChetanRaj0428-1998/index.html"
+  },
+  {
+    "revision": "532e4e9b521d836d3740",
+    "url": "/ChetanRaj0428-1998/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8e2272e61126534be766",
+    "url": "/ChetanRaj0428-1998/static/css/main.e550ce00.chunk.css"
+  },
+  {
+    "revision": "532e4e9b521d836d3740",
+    "url": "/ChetanRaj0428-1998/static/js/2.f55583a9.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/ChetanRaj0428-1998/static/js/2.f55583a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e2272e61126534be766",
+    "url": "/ChetanRaj0428-1998/static/js/main.4386c692.chunk.js"
+  },
+  {
+    "revision": "980a7c45ccc7b90a6700",
+    "url": "/ChetanRaj0428-1998/static/js/runtime-main.8a184621.js"
+  }
+]);
